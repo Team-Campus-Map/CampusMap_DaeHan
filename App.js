e77d2c75d711da8924208bfb2ed796434ec944c4
JONNAPUNCH <134913375+JONNAPUNCH@users.noreply.github.com>
2023-05-30 00:32:41 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapContainer from './MapContainer';
+
+function App() {
+  return (
+    <div>
+      <h1>Google Map</h1>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default App;
